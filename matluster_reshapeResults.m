c906348747ca032bf4eb2b8bbc14f0f76b6e8de0
Patrick Pletscher <pletscher@inf.ethz.ch>
2012-09-08 11:10:39 +0200

    support strings for the parameters and vectors for results

--- matluster_reshapeResults.m
+++ matluster_reshapeResults.m
@@ -16,13 +16,23 @@ for i=1:numel(table.report)
         for p_idx=1:numel(table.report{i}.params)
             n = table.report{i}.params{p_idx}.name;
             res.param_name{end+1} = n;
-            res.param_values{end+1} = [];
+            
+            v = table.report{i}.params{p_idx}.value;
+            if (~isstr(v))
+                res.param_values{end+1} = [];
+            else
+                res.param_values{end+1} = {};
+            end
         end
     end
     % add to the param_values
     for p_idx=1:numel(table.report{i}.params)
         v = table.report{i}.params{p_idx}.value;
-        res.param_values{p_idx}(end+1) = v;
+        if (~isstr(v))
+            res.param_values{p_idx}(end+1) = v;
+        else
+            res.param_values{p_idx}{end+1} = v;
+        end
     end
 end
 for p_idx=1:numel(res.param_values)
@@ -34,17 +44,31 @@ dims = [];
 for p_idx=1:numel(res.param_values)
     dims = [dims, numel(res.param_values{p_idx})];
 end
-res.results = zeros(dims);
+v = getfield(table.report{1}.result, result_fieldname);
+if (numel(v)==1)
+    res.results = zeros(dims);
+else
+    res.results = cell(dims);
+end
 
 % copy the results
 for i=1:numel(table.report)
     v = getfield(table.report{i}.result, result_fieldname);
     idx = [];
     for p_idx=1:numel(table.report{i}.params)
-        ind = find(res.param_values{p_idx} == table.report{i}.params{p_idx}.value);
+        temp = table.report{i}.params{p_idx}.value;
+        if (~isstr(temp))
+            ind = find(res.param_values{p_idx} == temp);
+        else
+            ind = find(strcmp(res.param_values{p_idx}, temp));
+        end
         idx = [idx, ind]; 
     end
     idx = mat2cell(idx, 1, ones(1,numel(idx)));
     idx = sub2ind(dims, idx{:});
-    res.results(idx) = v;
+    if (numel(v)==1)
+        res.results(idx) = v;
+    else
+        res.results{idx} = v;
+    end
 end
